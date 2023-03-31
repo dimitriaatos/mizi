48e7d28767bd0aca203501ea379313726920aeb0
M1Z1 <gmizithras@gmail.com>
2023-03-31 16:15:47 +0300

    images and content

--- renderers.js
+++
@@ -1,7 +0,0 @@
-export const Paragraphs = ({ content }) => {
-  return content.map((paragraph, index) => (
-    <p key={index} style={{ marginBottom: "0.5rem" }}>
-      {paragraph}
-    </p>
-  ));
-};
