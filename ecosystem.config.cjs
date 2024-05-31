12f28a3f7ef2fde74f96b39ab368ffa4e90bea46
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2024-05-31 13:29:47 +0300

    feat: pm2 deploy

---
+++ ecosystem.config.cjs
@@ -0,0 +1,31 @@
+const { loadEnvConfig } = require('@next/env')
+const env_production = loadEnvConfig(process.cwd()).combinedEnv
+
+const prod = {
+	user: 'dimitriaatos',
+	host: 'grain',
+	ref: 'origin/main',
+	repo: 'https://github.com/dimitriaatos/mizi.git',
+	path: '/home/dimitriaatos/sites/mizi/front',
+	'post-deploy':
+		'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.cjs --env production',
+	'ssh-options': 'ForwardAgent=yes',
+}
+
+module.exports = {
+	apps: [
+		{
+			name: 'mizi',
+			script: 'npm start',
+			env_production,
+		},
+	],
+
+	deploy: {
+		production: {
+			...prod,
+			'pre-deploy-local':
+				`rsync -v ./.env.production.local ${prod.user}@${prod.host}:${prod.path}/current/`,
+		},
+	},
+}
